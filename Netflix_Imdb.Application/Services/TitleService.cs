0b205b0945cb5af8c0f3f4ff281522b34532aab5
Diego Fernando Díaz Hernández <Ddiazh@intergrupo.com>
2024-05-22 22:52:43 -0500

    feat: add rediscache

--- Netflix_Imdb.Application/Services/TitleService.cs
+++ Netflix_Imdb.Application/Services/TitleService.cs
@@ -7,37 +7,93 @@ namespace Netflix_Imdb.Application.Services
     public class TitleService : ITitleService
     {
         private readonly ITitleRepository _titleRepository;
+        private readonly IRedisCache _redisCache;
 
-        public TitleService(ITitleRepository titleRepository)
+        public TitleService(ITitleRepository titleRepository, IRedisCache redisCache)
         {
             _titleRepository = titleRepository;
+            _redisCache = redisCache;
         }
 
         public async Task<(IEnumerable<MovieTitle> Titles, long Count)> GetAllTitlesAsync()
         {
+            var cacheKey = "all_titles";
+            var cachedTitles = await _redisCache.GetCacheData<IEnumerable<MovieTitle>>(cacheKey);
+
+            if (cachedTitles != null)
+            {
+                var count = cachedTitles.Count();
+                return (cachedTitles, count);
+            }
+
             var titles = await _titleRepository.GetAllTitlesAsync();
-            var count = await _titleRepository.GetTitleCountAsync();
-            return (titles, count);
+            var countFromDb = await _titleRepository.GetTitleCountAsync();
+
+          
+            await _redisCache.SetCacheData(cacheKey, titles, DateTimeOffset.Now.AddHours(1));
+
+            return (titles, countFromDb);
         }
 
         public async Task<(IEnumerable<MovieTitle> Titles, long Count)> SearchTitlesAsync(IEnumerable<string> genres, double? score)
         {
+            var cacheKey = $"search_titles_{string.Join(",", genres)}_{score}";
+            var cachedTitles = await _redisCache.GetCacheData<IEnumerable<MovieTitle>>(cacheKey);
+
+            if (cachedTitles != null)
+            {
+                var count = cachedTitles.Count();
+                return (cachedTitles, count);
+            }
+
             var titles = await _titleRepository.GetTitlesByCriteriaAsync(genres, score);
-            var count = titles.Count();
-            return (titles, count);
+            var countFromDb = titles.Count();
+
+          
+            await _redisCache.SetCacheData(cacheKey, titles, DateTimeOffset.Now.AddHours(1));
+
+            return (titles, countFromDb);
         }
 
         public async Task<(IEnumerable<MovieTitle> Titles, long Count)> GetTitlesByGenreAndRevenueAsync(IEnumerable<string> genres)
         {
+            var cacheKey = $"titles_by_genre_revenue_{string.Join(",", genres)}";
+            var cachedTitles = await _redisCache.GetCacheData<IEnumerable<MovieTitle>>(cacheKey);
+
+            if (cachedTitles != null)
+            {
+                var count = cachedTitles.Count();
+                return (cachedTitles, count);
+            }
+
             var titles = await _titleRepository.GetTitlesByGenreAndRevenueAsync(genres);
-            var count = titles.Count();
-            return (titles, count);
+            var countFromDb = titles.Count();
+
+       
+            await _redisCache.SetCacheData(cacheKey, titles, DateTimeOffset.Now.AddHours(1));
+
+            return (titles, countFromDb);
         }
+
         public async Task<(IEnumerable<MovieTitle> Titles, long Count)> GetAllTitlesByRevenueAsync()
         {
+            var cacheKey = "all_titles_by_revenue";
+            var cachedTitles = await _redisCache.GetCacheData<IEnumerable<MovieTitle>>(cacheKey);
+
+            if (cachedTitles != null)
+            {
+                var count = cachedTitles.Count();
+                return (cachedTitles, count);
+            }
+
             var titles = await _titleRepository.GetAllTitlesByRevenueAsync();
-            var count = titles.Count();
-            return (titles, count);
+            var countFromDb = titles.Count();
+
+        
+            await _redisCache.SetCacheData(cacheKey, titles, DateTimeOffset.Now.AddHours(1));
+
+            return (titles, countFromDb);
         }
     }
 }
+
